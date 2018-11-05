a80f97b540c4739747625272c55f5f87e0f9897d
backtake <stasiusoltysik@gmail.com>
2018-11-05 20:06:32 +0100

    Add FareRequest class to repo

--- src/main/java/com/example/demo/model/FareRequest.java
+++ src/main/java/com/example/demo/model/FareRequest.java
@@ -1,4 +1,17 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FareRequest {
+
+    private List<Fare> allFares = new ArrayList<>();
+
+    public List<Fare> getAllFares() {
+        return allFares;
+    }
+
+    public void setAllFares(List<Fare> allFares) {
+        this.allFares = allFares;
+    }
 }
