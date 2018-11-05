4e6a7abbda95db63d809993f45647821f82c9d19
backtake <stasiusoltysik@gmail.com>
2018-11-05 20:07:05 +0100

    Add interface MarketProvider to repo

--- src/main/java/com/example/demo/service/MarketProvider.java
+++ src/main/java/com/example/demo/service/MarketProvider.java
@@ -1,12 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Fare;
+import com.example.demo.model.FareRequest;
 import com.example.demo.model.Market;
 
 import java.util.List;
 
 public interface MarketProvider {
 
-    void addFaresData(Fare fare);
+    void addFaresData(FareRequest fareRequest);
     List<Market> getAllMarkets();
 }
