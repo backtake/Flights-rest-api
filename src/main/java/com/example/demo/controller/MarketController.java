b3975149bd7352c6e91744f014cb9bd3b7c06e54
backtake <stasiusoltysik@gmail.com>
2018-11-05 14:56:18 +0100

    Add interface to repo

--- src/main/java/com/example/demo/controller/MarketController.java
+++ src/main/java/com/example/demo/controller/MarketController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.FareRequest;
-import com.example.demo.model.MarketResponse;
+import com.example.demo.model.Market;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public interface MarketController {
 
-    MarketResponse index();
+    List<Market> index();
     void addFares(@RequestBody FareRequest fareRequest);
 }
