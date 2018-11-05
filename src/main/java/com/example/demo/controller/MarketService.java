7efb927b199b0c56f285b0898110815fe2df1ee0
backtake <stasiusoltysik@gmail.com>
2018-11-05 14:50:36 +0100

    Create most service functionality

--- src/main/java/com/example/demo/controller/MarketService.java
+++ src/main/java/com/example/demo/controller/MarketService.java
@@ -1,4 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MarketService {
+
+    public void addFaresToAll(FareRequest fareRequest) {
+        List<Fare> fares = fareRequest.getFares();
+        AllFares.getAllFares().addAll(fares);//teoretycznie może się przydać do wyświetlania wszystkich fare
+
+        List<Market> markets = createMarketsOutOfFares(fares);
+        getAllMarkets().addAll(markets);
+    }
+
+    private List<Market> createMarketsOutOfFares(List<Fare> fares) {
+        return null;
+    }
+
+    public List<Market> getAllMarkets() {
+        return MarketResponse.getAllMarkets();
+    }
+
+    private Market mapFareToMarket(Fare fare) {
+        String marketId = createMarketId(fare);
+
+        return null;
+    }
+
+    private String createMarketId(Fare fare) {
+        return fare.getOrigin() + "[" + fare.getDate().trim() + "]" + fare.getDestination();
+    }
 }
